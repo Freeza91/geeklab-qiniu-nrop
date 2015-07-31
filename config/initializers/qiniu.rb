a738d8ec8b044d2590a4c95e859f30384b78aaad
rudy <347212291@qq.com>
2015-07-31 11:15:26 +0800

    fix qiniu

--- config/initializers/qiniu.rb
+++ config/initializers/qiniu.rb
@@ -2,5 +2,5 @@ require 'qiniu'
 
 MyApp.configure do |app|
   Qiniu.establish_connection! :access_key => app.settings.qiniu_access_key,
-                              :secret_key => app.settings.qiniu_access_key
+                              :secret_key => app.settings.qiniu_secret_key
 end
