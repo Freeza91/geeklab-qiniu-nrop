f4e6b78017015703a851424e9eaa7a118e2e794b
rudy <347212291@qq.com>
2015-08-04 17:14:50 +0800

    update assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -11,17 +11,20 @@ class AssignmentsController < MyApp
 
     if params[:code].to_s == '0' && tester = Tester.find_by(id: params[:tester_id])
       if assignment = tester.assignments.find_by(id: params[:assignment_id])
-        # key = params[:items].first[:key]
-        key = '33.jpg'
+        key = params[:items].first[:key]
         assignment.update_attribute(:is_sexy, true) if SoraAoi(key)
         code, result, response_headers = Qiniu::Storage.delete(
           'video-images',
           key
         )
+        "Thanks SoraAoi"
+      else
+        "没有找到assignment"
       end
+    else
+      "没有找到tester"
     end
 
-    "Thanks SoraAoi"
   end
 
 end
